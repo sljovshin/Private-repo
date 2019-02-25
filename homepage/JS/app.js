b17ac74b649e3c9ed4eab1b3783ec5037c142e2b
sljovshin <47101786+sljovshin@users.noreply.github.com>
2019-02-25 16:08:43 +0100

    work on open animation

--- homepage/JS/app.js
+++ homepage/JS/app.js
@@ -1,36 +1,38 @@
 var left_o = document.getElementById('l');
 var right_o = document.getElementById('r');
 var center_o = document.getElementById('c');
+var open_bg = document.getElementsByClassName('open_bg')
 var cto = document.getElementById('click_to_open');
-
+var clicked = false;
 cto.addEventListener ("click", open_ses);
 
 function open_ses() {
+
+  if (clicked == false) {
   left_o.style.position = "absolute"
   right_o.style.position = "absolute"
   var pos = 30;
-  var id = setInterval(frame, 5);
-  var anim_done = false;
-  function frame() {
+  var id1 = setInterval(frame1, 10);
+  var anim_done1 = false;
+  function frame1() {
    if (pos < 4) {
-     clearInterval(id);
-     anim_done = true;
-   } else {
-     pos--;
+     clearInterval(id1);
+     anim_done1 = true;
+   }
+   else {
+     pos = pos - 0.7;
      left_o.style.left = pos + "%";
      right_o.style.right = pos + "%";
     }
-  }
-  if (anim_done == true) {
-    function fadeOut() {
-      var fadeEffect = setInterval(fadeEffect, 5);
-      function fadeEffect()
-         if (center_o.style.opacity > 0) {
-             center_o.style.opacity -= 0.1;
-         } else {
-             clearInterval(fadeEffect);
-         }
+    var id2 = setInterval(frame2, 1);
+    function frame2() {
+      if (anim_done1 == true) {
+        center_o.style.opacity --;
+      }
     }
+    clicked = true;
   }
+ }
+}
 
 cto.addEventListener ("click", open_ses);
