907c85558a59d160f1f84fe06d35383710917d08
sljovshin <47101786+sljovshin@users.noreply.github.com>
2019-03-05 16:40:11 +0100

    edited css

--- drag-and-drop/JS/app.js
+++ drag-and-drop/JS/app.js
@@ -1,11 +1,17 @@
 var item_panel = document.getElementById('item_panel');
-var items = [1,2,3,4,5,6,8,9,10]
+var main_view = document.getElementById('main_view');
+var items_in_panel = [1,2,3,4,5,6,]
+var items_in_view = [7,8,9,10,11,12]
 
 
 /*  Populate Item Panel based on items array*/
-for (var i = 0; i < items.length; i++) {
-    var item = `<div class="item" draggable="true" ondragstart="drag(event)"> <p> ${items[i]} </p> </div>`
-    item_panel.innerHTML = item_panel.innerHTML + item;
+for (var i = 0; i < items_in_panel.length; i++) {
+    var item_in_panel = `<div class="item_in_panel" draggable="true" ondragstart="drag(event)"> <p> ${items_in_panel[i]} </p> </div>`;
+    item_panel.innerHTML = item_panel.innerHTML + item_in_panel;
+}
+for (var i = 0; i < items_in_view.length; i++) {
+    var item_in_view = `<div class="item_in_view" draggable="true" ondragstart="drag(event)"> <p> ${items_in_view[i]} </p> </div>`;
+    main_view.innerHTML = main_view.innerHTML + item_in_view;
 }
 function allowDrop(ev) {
   ev.preventDefault();
