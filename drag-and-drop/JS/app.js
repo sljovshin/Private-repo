2679d1ca09ecc221adbfa22f532cd5d07f625096
sljovshin <47101786+sljovshin@users.noreply.github.com>
2019-03-07 14:03:55 +0100

    .

--- drag-and-drop/JS/app.js
+++ drag-and-drop/JS/app.js
@@ -6,23 +6,40 @@ var items_in_view = [7,8,9,10,11,12]
 
 /*  Populate Item Panel based on items array*/
 for (var i = 0; i < items_in_panel.length; i++) {
-    var item_in_panel = `<div class="item_in_panel" draggable="true" ondragstart="drag(event)"> <p> ${items_in_panel[i]} </p> </div>`;
+    var item_in_panel = `<div class="item_in_panel" draggable="true" ondragstart="dragstart_handler(event);"> <p> ${items_in_panel[i]} </p> </div>`;
     item_panel.innerHTML = item_panel.innerHTML + item_in_panel;
 }
 for (var i = 0; i < items_in_view.length; i++) {
-    var item_in_view = `<div class="item_in_view" draggable="true" ondragstart="drag(event)"> <p> ${items_in_view[i]} </p> </div>`;
+    var item_in_view = `<div class="item_in_view" draggable="true" ondragstart="dragstart_handler(event);"> <p> ${items_in_view[i]} </p> </div>`;
     main_view.innerHTML = main_view.innerHTML + item_in_view;
 }
+
+
+
 function allowDrop(ev) {
   ev.preventDefault();
 }
 
-function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
+
+function dragstart_handler(ev) {
+ console.log("dragStart");
+ // Change the source element's background color to signify drag has started
+ ev.currentTarget.style.opacity = "0.7";
+ // Set the drag's format and data. Use the event target's id for the data
+ ev.dataTransfer.setData("text/plain", ev.target.id);
 }
 
-function drop(ev) {
-  ev.preventDefault();
-  var data = ev.dataTransfer.getData("text");
-  ev.target.appendChild(document.getElementById(data));
+function dragover_handler(ev) {
+ console.log("dragOver");
+ ev.preventDefault();
+}
+
+function drop_handler(ev) {
+ console.log("Drop");
+ ev.preventDefault();
+ // Get the data, which is the id of the drop target
+ var data = ev.dataTransfer.getData("text");
+ ev.target.appendChild(document.getElementById(data));
+ // Clear the drag data cache (for all formats/types)
+ ev.dataTransfer.clearData();
 }
