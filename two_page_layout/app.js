a43a43799f38aa60251d226b3c062ae9e1f385e5
sljovshin <47101786+sljovshin@users.noreply.github.com>
2019-03-08 16:55:43 +0100

    working on menu links and iframe

--- two_page_layout/app.js
+++ two_page_layout/app.js
@@ -3,9 +3,20 @@ var mainpage = document.getElementById('main');
 var casepage = document.getElementById('case');
 var left_nav = document.getElementById('nav1');
 var right_nav = document.getElementById('nav2');
+var case_item = document.getElementsByClassName('case_item');
+var case_canvas = document.getElementById('case_canvas')
 
 console.log("test");
 
+for (var i = 0; i < case_item.length; i++) {
+  case_item[i].addEventListener('click', openLink);
+}
+
+function openLink(){
+  var newlink = this.case_item.getAttribute('data-value');
+  console.log(`this is: ${newlink}`);
+  case_canvas.src = newlink;
+}
 
 function teaseCase() {
   mainpage.style.left = "-15%";
